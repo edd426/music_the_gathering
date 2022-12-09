571c9dc160f9f0b43daff14ab842ce8b242690bf
Evan DeLord <eddelord@gmail.com>
2022-12-09 16:10:04 -0600

    adding a deck class

---
+++ deck.py
@@ -0,0 +1,16 @@
+class Deck:
+    def __init__(self, cards):
+        self.cards = cards
+        self.shuffle()
+
+    def shuffle(self):
+        # shuffle the cards in the deck
+        random.shuffle(self.cards)
+
+    def draw(self):
+        # draw a card from the top of the deck
+        return self.cards.pop()
+
+    def remaining(self):
+        # return the number of cards remaining in the deck
+        return len(self.cards)
